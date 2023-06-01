0ee1662a8f48b4eb83f81cf6c02d8372150c04a6
Mamun172136 <ma1536204@gmail.com>
2023-06-01 19:36:34 +0600

    done create  user interface and model

--- src/server.ts
+++ src/server.ts
@@ -1,18 +1,17 @@
-import mongoose from "mongoose";
-import app from './app';
+import mongoose from 'mongoose'
+import app from './app'
 import config from './config.ts/index'
 async function main() {
-   try {
-    await mongoose.connect(config.database_url as string );
+  try {
+    await mongoose.connect(config.database_url as string)
     console.log(`database successfully connected`)
     app.listen(config.port, () => {
-        console.log(`Example app listening on port ${config.port}`)
-      })
-   } catch (error) {
+      console.log(`Example app listening on port ${config.port}`)
+    })
+  } catch (error) {
     console.log(`database error is happening`, error)
-    
-   }
-  
-    // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
   }
-  main();
+
+  // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
+}
+main()
